a62c2fda80c6d433fa81d98d40545bf75828d861
JonnathanThompson <143512214+JonnathanThompson@users.noreply.github.com>
2023-09-19 09:37:59 -0400

    Add files via upload

--- code.py
+++ code.py
@@ -1,36 +1,37 @@
-import time
-import board
-import digitalio
-import pwmio
+import time #imports time libary that are needed to run this code
+import board #imports board libary that are needed to run this code
+import digitalio #imports digitalio libary that are needed to run this code
+import pwmio #imports libaries that are needed to run this code
 
-from adafruit_motor import motor
+from adafruit_motor import motor #imports a small section of adafruit_motor libary
 
-PWM_MLA = board.GP12
-PWM_MLB = board.GP13
+left_motor_forward = board.GP12 #Initilizes the varibale left_motor_forward and attaches it to GP12
+left_motor_backwards = board.GP13 #Initilizes the varibale left_motor_backwards and attaches it to GP13
 
-pwm_La = pwmio.PWMOut(PWM_MLA, frequency=10000)
-pwm_Lb = pwmio.PWMOut(PWM_MLB, frequency=10000)
+pwm_La = pwmio.PWMOut(left_motor_forward, frequency=10000) #Tells the pico that this component is an output (and some other configuration)
+pwm_Lb = pwmio.PWMOut(left_motor_backwards, frequency=10000) #Tells the pico that this component is an output (and some other configuration)
 
-Left_Motor = motor.DCMotor(pwm_La, pwm_Lb)
-Left_Motor_speed = 0
+Left_Motor = motor.DCMotor(pwm_La, pwm_Lb) #Intilaizes Left_Motor and configuration line and it is required
+Left_Motor_speed = 0 #Intilaizes the variable Left_Motor_speed to 0
 
 
-PWM_MLA = board.GP14
-PWM_MLB = board.GP15
+right_motor_forward = board.GP14 #Initilizes the varibale right_motor_forward and attaches it to GP14
+right_motor_backwards = board.GP15 #Initilizes the varibale right_motor_backwards and attaches it to GP15
 
-pwm_La = pwmio.PWMOut(PWM_MLA, frequency=10000)
-pwm_Lb = pwmio.PWMOut(PWM_MLB, frequency=10000)
 
-Right_Motor = motor.DCMotor(pwm_La, pwm_Lb)
-Right_Motor_speed = 0
+pwm_La = pwmio.PWMOut(right_motor_forward, frequency=10000) #Tells the pico that this component is an output (and some other configuration)
+pwm_Lb = pwmio.PWMOut(right_motor_backwards, frequency=10000) #Tells the pico that this component is an output (and some other configuration)
+
+Right_Motor = motor.DCMotor(pwm_La, pwm_Lb) #Intilaizes Right_Motor and configuration line and it is required
+Right_Motor_speed = 0 #Intilaizes the variable Right_Motor_speed to 0
 
 
 
 
 while True:
 
-    Left_Motor_speed = -1
-    Left_Motor.throttle = Left_Motor_speed
+    Left_Motor_speed = -1 #Left motor moves backward
+    Left_Motor.throttle = Left_Motor_speed #put the throttle on the speed
     Right_Motor_speed = -1
     Right_Motor.throttle = Right_Motor_speed
     time.sleep(2)
