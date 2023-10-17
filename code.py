75ca0ee1a4d319bc43c3dbc2a229ebf574241f21
JonnathanThompson <143512214+JonnathanThompson@users.noreply.github.com>
2023-10-17 09:50:14 -0400

    Update code.py

--- code.py
+++ code.py
@@ -25,7 +25,41 @@ pwm_Lb = pwmio.PWMOut(right_motor_backwards, frequency=10000) #Tells the pico th
 Right_Motor = motor.DCMotor(pwm_La, pwm_Lb) #Intilaizes Right_Motor and configuration line and it is required
 Right_Motor_speed = 0 #Intilaizes the variable Right_Motor_speed to 0
 
+def Robot_forward
+    Left_Motor_speed = -1 
+    Left_Motor.throttle = Left_Motor_speed
+    Right_Motor_speed = -1
+    Right_Motor.throttle = Right_Motor_speed
+    time.sleep(3)
+def Robot_backwards
+    Left_Motor_speed = 1 
+    Left_Motor.throttle = Left_Motor_speed
+    Right_Motor_speed = 1 
+    Right_Motor.throttle = Right_Motor_speed
+    time.sleep(3)
 
+
+def Robot_right
+    Left_Motor_speed = 1
+    Left_Motor.throttle = Left_Motor_speed
+    Right_Motor_speed = -1 
+    Right_Motor.throttle = Right_Motor_speed
+    time.sleep(3)
+
+
+def Robot_left
+   Left_Motor_speed = -1
+    Left_Motor.throttle = Left_Motor_speed
+    Right_Motor_speed = 1 
+    Right_Motor.throttle = Right_Motor_speed
+    time.sleep(3)
+
+def Robot_stop
+   Left_Motor_speed = 0 
+    Left_Motor.throttle = Left_Motor_speed
+    Right_Motor_speed = 0
+    Right_Motor.throttle = Right_Motor_speed
+    time.sleep(3)
 
 
 while True:
